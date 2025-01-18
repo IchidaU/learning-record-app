a1c09a302c65aa77f77f0a391be84705479836fe
IchidaU <koo.be.koo@gmail.com>
2025-01-18 22:52:26 +0900

    sleep関数削除、returnの条件分岐改善

--- src/components/LogContent.jsx
+++ src/components/LogContent.jsx
@@ -4,15 +4,11 @@ import { getAllLogs } from '../utils/supabaseFunctions';
 export const LogContent = () => {
   const [logs, setLogs] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const sleep = (ms) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  };
 
   useEffect(() => {
     const getLogs = async () => {
       try {
         setIsLoading(true);
-        sleep(3000);
         const fetchedLogs = await getAllLogs();
         setLogs(fetchedLogs);
       } catch (error) {
@@ -24,18 +20,16 @@ export const LogContent = () => {
     getLogs();
   }, []);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div>
-        <h2>学習記録一覧</h2>
-        {logs.map((log) => (
-          <p key={log.id}>
-            {log.title} {log.time}時間
-          </p>
-        ))}
-      </div>
-    );
-  }
+  return isLoading ? (
+    <div>Loading...</div>
+  ) : (
+    <div>
+      <h2>学習記録一覧</h2>
+      {logs.map((log) => (
+        <p key={log.id}>
+          {log.title} {log.time}時間
+        </p>
+      ))}
+    </div>
+  );
 };
