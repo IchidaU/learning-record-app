16141293c6dcce37360335de2cad26b7197c56f9
IchidaU <koo.be.koo@gmail.com>
2025-02-08 23:49:47 +0900

    firebase.json, vite.config.js修正

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import env from "vite-plugin-env-compatible";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import env from 'vite-plugin-env-compatible';
 
 export default defineConfig({
-    plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
+  plugins: [react(), env({ prefix: 'VITE', mountedPath: 'process.env' })],
+  base: '/',
 });
